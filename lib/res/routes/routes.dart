382641e5663e2012f36cd746054d5ad9cf073bc7
ahammed hinas <82311707+hinasahammed@users.noreply.github.com>
2024-09-17 10:19:33 +0530

    ipdated packages

--- lib/res/routes/routes.dart
+++ lib/res/routes/routes.dart
@@ -3,6 +3,7 @@ import 'package:mobies/res/routes/toutes_name.dart';
 import 'package:mobies/view/home_view.dart';
 
 class Routes {
-  static appRoutes() =>
-      [GetPage(name: RoutesName.home, page: () => const HomeView())];
+  static appRoutes() => [
+        GetPage(name: RoutesName.home, page: () => const HomeView()),
+      ];
 }
